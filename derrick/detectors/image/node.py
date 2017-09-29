62c047ec899427824152e56025e99c2bfa45a948
ringtail <zhongwei.lzw@alibaba-inc.com>
2017-09-29 13:52:46 +0800

    fix derrick docker-compose.yml

--- derrick/detectors/image/node.py
+++ derrick/detectors/image/node.py
@@ -17,11 +17,11 @@ NODEJS_LATEST = "node:latest"
 class NodeVersionDetector(Detector):
     def execute(self):
         try:
-            output = subprocess.check_output(["nodejs", "--version"], shell=False)
+            output = subprocess.check_output(["node", "--version"], shell=False)
             Logger.debug("NodeJs version detected is %s" % output)
             version = NodeVersionDetector.get_most_relative_version(output)
         except:
-            version = NODEJS_LATEST
+            version = NODEJS_8
 
         return {"version": version}
 
@@ -30,7 +30,7 @@ class NodeVersionDetector(Detector):
         version_num = str(version)[1:]
         version_arr = version_num.split(".")
 
-        detect_version = NODEJS_LATEST
+        detect_version = NODEJS_8
         try:
             base_version = version_arr[0]
             if base_version == "4":
