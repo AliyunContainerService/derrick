4c3d37ff4eb6a2a21c25a74b5018680d96e694c4
ringtail <zhongwei.lzw@alibaba-inc.com>
2017-10-13 10:46:01 +0800

    win32api support

--- derrick-0.0.9/derrick/detectors/package_manager/npm.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import, division, print_function
-from derrick.core.detector import Detector
-
-class NpmDetector(Detector):
-    def execute(self, *args, **kwargs):
-        pass 
