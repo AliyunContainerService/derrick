8dfe5afc6d574df0fd6b8bb4ecfb0c908bfb6a24
ringtail <zhongwei.lzw@alibaba-inc.com>
2017-10-16 16:42:14 +0800

    add travis.ci

--- tests/test_core_command_manager.py
+++ tests/test_core_command_manager.py
@@ -19,7 +19,7 @@ class CommandManagerTestCase(unittest.TestCase):
 
     def test_built_in_commands_registered(self):
         commands_dict = self.cm.all()
-        self.assertIsNot(None, commands_dict["build"])
+        self.assertIsNot(None, commands_dict["up"])
         self.assertIsNot(None, commands_dict["init"])
 
     def test_built_int_commands_re_registration(self):
