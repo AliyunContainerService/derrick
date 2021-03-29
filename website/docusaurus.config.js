27ea19da98aa0d8a1b7226d8e80c81a3905cc0ae
Hongchao Deng <hongchaodeng1@gmail.com>
2021-03-29 01:38:08 -0400

    update copyright

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -74,7 +74,7 @@ module.exports = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()}. Built with Docusaurus.`,
     },
   },
   presets: [
